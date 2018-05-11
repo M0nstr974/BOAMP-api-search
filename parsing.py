334fa9d662fbaee94abbd92d68dbd9f9773c7894
Raph A <raphyninho@hotmail.fr>
2018-05-11 20:31:42 +0200

    index last 1000 offers from json mapping

--- parsing.py
+++ parsing.py
@@ -1,22 +1,32 @@
 import requests
-import json
 from elasticsearch import Elasticsearch
+import traceback
+import json
 
-curseur = 1
-id_offre = 1
-
-response = requests.get("http://api.dila.fr/opendata/api-boamp/annonces/search?criterion=securite&curseur=" + str(curseur)).json()
+with open('mapping_offres.json') as f:
+    mapping = json.load(f)
 
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-for offre in response["item"]:
-    es.index(index='offres', doc_type='offre', id=id_offre, body=offre)
-    id_offre += 1
+es.indices.create(index='test-offres3', ignore=400, body=mapping)
 
-""" while True:
-    response = requests.get("http://api.dila.fr/opendata/api-boamp/annonces/search?criterion=securite&curseur=" + str(curseur))
-    if response.status_code != 200:
-        break
-    else:
-        items.update(response.json())
-    curseur += 1000 """
+max_offres = 1000
+id_annonce = 1
 
+response = requests.get("http://api.dila.fr/opendata/api-boamp/annonces/search?criterion=securite")
+curseur_limit = response.json()["nbItemsExistants"] - max_offres -1 
+curseur = response.json()["nbItemsExistants"] - 1000 #response contient max 1000 annonces.
+
+try:
+    while curseur > curseur_limit:
+        #pour chaque annonce, l'ajouter à l'index
+        for offre in response.json()["item"]:
+            annonce = requests.get('http://api.dila.fr/opendata/api-boamp/annonces/v230/' + offre["value"])
+            id_offre = offre["value"]
+            if annonce.status_code == 200:
+                es.index(index='test-offres3', doc_type='annonce', id=id_annonce, body=annonce.json())
+                id_annonce += 1
+        curseur -= 1000
+        response = requests.get("http://api.dila.fr/opendata/api-boamp/annonces/search?criterion=securite&curseur=" + str(curseur))
+except:
+    traceback.print_exc()
+    print('Error at \'id_offre\' = ' + str(id_offre) + '\n')
